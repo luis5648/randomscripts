5ee4a2fca539b5534ba6718844218244b2461a9a
l <l@localhost.localdomain>
2021-10-31 20:06:32 -0600

    comment lines

--- bash/rolling-debian-upgrade.sh
+++ bash/rolling-debian-upgrade.sh
@@ -6,3 +6,6 @@ apt full-upgrade
 apt update
 apt full-upgrade
 apt autoremove
+#there are repeated instructions cause sometimes 
+#the rolling dist based in debian doesnt install all the
+#updates at the first time.
